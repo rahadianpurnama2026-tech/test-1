2eead88ed17543f59a19443e7eabb1c5e2f033dc
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-07-15 21:55:13 +0700

    Extend saklar function to support 7 toggles and lamps
    Added support for additional toggles and lamps, updating background color based on their states.

--- saklar.js
+++ saklar.js
@@ -2,10 +2,18 @@ function saklar() {
   let toggle1 = document.getElementById("default-toggle1")
   let toggle2= document.getElementById("default-toggle2")
   let toggle3 = document.getElementById("default-toggle3")
+  let toggle4 = document.getElementById("default-toggle4")
+  let toggle5 = document.getElementById("default-toggle5")
+  let toggle6 = document.getElementById("default-toggle6")
+  let toggle7 = document.getElementById("default-toggle7")
 
   let lampu1 = document.getElementById("lampu1");
   let lampu2 = document.getElementById("lampu2");
   let lampu3 = document.getElementById("lampu3");
+  let lampu4 = document.getElementById("lampu4");
+  let lampu5 = document.getElementById("lampu5");
+  let lampu6 = document.getElementById("lampu6");
+  let lampu7 = document.getElementById("lampu7");
 
 
   if (toggle1.checked) {
@@ -25,4 +33,19 @@ function saklar() {
   } else {
       lampu3.src = "assets/images/off.gif";
   }
+
+  // Cek apakah ada lampu yang menyala
+if (
+    toggle1.checked ||
+    toggle2.checked ||
+    toggle3.checked ||
+    toggle4.checked ||
+    toggle5.checked ||
+    toggle6.checked ||
+    toggle7.checked
+) {
+    document.body.style.backgroundColor = "#fef08a";
+} else {
+    document.body.style.backgroundColor = "#1f2937";
+}
 } 
