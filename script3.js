9e5f29d46ee59248296a593b0a80643756aa4bf8
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-01 22:07:10 +0700

    Enhance video controls with auto-hide feature

--- script3.js
+++ script3.js
@@ -1,28 +1,40 @@
+const container = document.getElementById("videoContainer");
 const video = document.getElementById("myVideo");
-const progress = document.getElementById("progress")
+const progress = document.getElementById("progress");
+
+let hideTimer;
+
+function showControls() {
+    container.classList.add("show-controls");
+    clearTimeout(hideTimer);
+    hideTimer = setTimeout(() =>{
+        container.classList.remove("show-controls");
+    },3000);
+}
+
+container.addEventListener("mousemove",showControls);
+container.addEventListener("touchstart",showControls);
+
+video.addEventListener("timeupdate", () => {
+    const value = (video.currentTime / video.duration) * 100;
+    progress.value = value;
+});
 
     function playVideo () {
         video.play();
+        showControls();
     }
+
     function pauseVideo () {
         video.pause();
+        showControls();
     }
+    
     function minimizeVideo () {
+        showControls();
         if(video.style.maxWidth == "400px"){
             video.style.maxWidth = "720px";
         } else{
             video.style.maxWidth = "400px";
         }
     }
-
-    video.addEventListener("timeupdate", () => {
-        const percent =
-            (video.currentTime / video.duration) * 100;
-        progress.value = percent;
-    });
-
-    progress.addEventListener("input", () => {
-        const waktu =
-            (progress.value / 100) * video.duration;
-        video.currentTime = waktu;
-    });
