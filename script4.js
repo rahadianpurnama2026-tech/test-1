438927bb7ef9a94d0457b0722fc424394b8be862
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-02 21:36:44 +0700

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,9 @@
+let nama = "Rahadian";
+const umur = 20;
+let hobi = "Coding";
+
+console.log(nama);
+console.log(umur);
+console.log(hobi);
+
+alert("Halo, nama saya " + nama + ", umur saya " + umur + " tahun, dan hobi saya adalah " + hobi);
