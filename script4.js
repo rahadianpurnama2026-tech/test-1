e3de70a973a2b94468a59874c73aa698695ae594
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-03 20:49:24 +0700

    Add new variables and type logging to script4.js
    Added new variables for 'namaBaru', 'umurBaru', and 'lulus'. Included console logs for new variables' types.

--- script4.js
+++ script4.js
@@ -2,8 +2,17 @@ let nama = "Rahadian";
 const umur = 20;
 let hobi = "Coding";
 
+let namaBaru = "Rndzhx";
+let umurBaru = 25;
+let lulus = true;
+
 console.log(nama);
 console.log(umur);
 console.log(hobi);
+
+console.log(typeof namaBaru);
+console.log(typeof umurBaru);
+console.log(typeof lulus);
+
 
 alert("Halo, nama saya " + nama + ", umur saya " + umur + " tahun, dan hobi saya adalah " + hobi);
