4b57a56866e7555994ae97ff3dddcae9dc81dc60
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-04 23:46:26 +0700

    Refactor script4.js to add arithmetic functions

--- script4.js
+++ script4.js
@@ -16,3 +16,35 @@ console.log(typeof lulus);
 
 
 alert("Halo, nama saya " + nama + ", umur saya " + umur + " tahun, dan hobi saya adalah " + hobi);
+
+function tambah() {
+    let angka1 = Number(document.getElementById("angka1").value);
+    let angka2 = Number(document.getElementById("angka2").value);
+    let hasil = angka1 + angka2;
+
+    document.getElementById("hasil").innerHTML = `Hasil: ${hasil}`;
+}
+
+function kurang() {
+    let angka1 = Number(document.getElementById("angka1").value);
+    let angka2 = Number(document.getElementById("angka2").value);
+    let hasil = angka1 - angka2;
+
+    document.getElementById("hasil").innerHTML = `Hasil: ${hasil}`;
+}
+
+function kali() {
+    let angka1 = Number(document.getElementById("angka1").value);
+    let angka2 = Number(document.getElementById("angka2").value);
+    let hasil = angka1 * angka2;
+
+    document.getElementById("hasil").innerHTML = `Hasil: ${hasil}`;
+}
+
+function bagi() {
+    let angka1 = Number(document.getElementById("angka1").value);
+    let angka2 = Number(document.getElementById("angka2").value);
+    let hasil = angka1 / angka2;
+
+    document.getElementById("hasil").innerHTML = `Hasil: ${hasil}`;
+}
