6d4b23922c2368219910d6f04f4d02f326359c3a
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-05 21:46:42 +0700

    Implement graduation check function
    Added a function to check graduation status based on input score.

--- script4.js
+++ script4.js
@@ -15,8 +15,9 @@ console.log(typeof umurBaru);
 console.log(typeof lulus);
 
 
-alert("Halo, nama saya " + nama + ", umur saya " + umur + " tahun, dan hobi saya adalah " + hobi);
+alert(`Halo, nama saya ${nama}, umur saya ${umur} tahun, dan hobi saya adalah ${hobi}`);
 
+// Kalkulator sederhana
 function tambah() {
     let angka1 = Number(document.getElementById("angka1").value);
     let angka2 = Number(document.getElementById("angka2").value);
@@ -48,3 +49,19 @@ function bagi() {
 
     document.getElementById("hasil").innerHTML = `Hasil: ${hasil}`;
 }
+
+// Cek Kelulusan
+function cek() {
+    let nilai = Number(document.getElementById("nilai").value);
+
+    if (nilai < 0 || nilai > 100) {
+        document.getElementById("hasil1").innerHTML = "Nilai tidak valid. Silahkan masukkan nilai antara 0-100.";
+    } 
+    else if (nilai >= 75) {
+        document.getElementById("hasil1").innerHTML = "Selamat, anda Lulus!";
+    } else {
+        document.getElementById("hasil1").innerHTML = "Maaf, anda Tidak Lulus.";
+    }
+}
+
+// Tugas hari 5 (Sistem Grade, Cek Usia)
