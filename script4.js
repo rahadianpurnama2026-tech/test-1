cd8424c5c67866c1ba99c2c1f635d84fbd05704b
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-11 23:03:31 +0700

    Refactor script4.js for clarity and functionality

--- script4.js
+++ script4.js
@@ -56,8 +56,7 @@ function cek() {
 
     if (nilai < 0 || nilai > 100) {
         document.getElementById("hasil1").innerHTML = "Nilai tidak valid!";
-    }
-    else {
+    } else {
 
         if (nilai >= 75) {
             document.getElementById("hasil1").innerHTML = "Selamat, anda Lulus!";
@@ -84,4 +83,25 @@ function cek() {
     }
 }
 
-// Tugas hari 5 (Sistem Grade, Cek Usia)
+// Cek Usia
+function usia() {
+    let usia = Number(document.getElementById("usia").value);
+
+    if (usia < 0 || usia > 100) {
+        document.getElementById("hasil3").innerHTML = "Usia tidak valid!";
+    } else {
+        if (usia >= 0 && usia <= 2) {
+            document.getElementById("hasil3").innerHTML = "Anda masih bayi!";
+        } else if (usia >= 3 && usia <= 5) {
+            document.getElementById("hasil3").innerHTML = "Anda masih balita!";
+        } else if (usia >= 6 && usia <= 12) {
+            document.getElementById("hasil3").innerHTML = "Anda anak-anak!";
+        } else if (usia >= 13 && usia <= 19) {
+            document.getElementById("hasil3").innerHTML = "Anda remaja!";
+        } else if (usia >= 20 && usia <= 59) {
+            document.getElementById("hasil3").innerHTML = "Anda sudah dewasa!";
+        } else {
+            document.getElementById("hasil3").innerHTML = "Anda sudah Lansia!";
+        }
+    }
+}
