db335e165734c52af9bc7d1198c89f8c896f13f0
rahadianpurnama2026-tech <rahadianpurnama2026@gmail.com>
2026-08-18 22:29:46 +0700

    Refactor script4.js for improved functionality

--- script4.js
+++ script4.js
@@ -105,3 +105,108 @@ function usia() {
         }
     }
 }
+
+// Challenge hari 4 && 5 (Kalkulator Simple)
+
+ function hitung() {
+    let a = Number(document.getElementById("angka3").value);
+    let b = Number(document.getElementById("angka4").value);
+    let operator = document.getElementById("operator").value;
+
+    if (operator === "+") {
+        return a + b;
+    } else if (operator === "-") {
+        return a - b;
+    } else if (operator === "*") {
+        return a * b;
+    } else if (operator === "/") {
+        return a / b;
+    } else {
+        return "Operator tidak valid";
+    }
+}
+
+function hasil() {
+    let hasilHitung = hitung();
+
+    document.getElementById("hasil4").innerHTML = "Hasil: " + hasilHitung;
+}
+
+
+// Challenge hari 6
+
+function buatProfil(nama, umur) {
+    const namaSaya = "Nama: " + nama;
+    const umurSaya = "Umur: " + umur;
+    
+        return namaSaya + "\n" + umurSaya + "tahun"; 
+}
+
+    let profil = buatProfil("Rndzhx", 25);
+
+    console.log(profil);
+
+// Challenge hari 7 (Array)
+
+let nilai1 = [65, 80, 95, 70, 88];
+let nilaiTertinggi = 0;
+
+for (let i = 0; i < nilai1.length; i++) {
+    if (nilai1[i] > nilaiTertinggi) {
+        nilaiTertinggi = nilai1[i];
+    } 
+}
+
+console.log("Nilai tertinggi "+ nilaiTertinggi);
+
+// Challenge hari 8 (Array + Method)
+
+let nama1 = ["Rahadian", "Andi", "Budi", "Citra"];
+
+if (nama1.includes("Rahadian")) {
+    console.log("Rahadian ditemukan!");
+} else {
+    console.log("Rahadian tidak ditemukan!");
+}
+
+console.log(nama1.indexOf("Deni"));
+
+
+let teman = ["Andi", "Budi", "Citra", "Deni"];
+
+let posisi = teman.indexOf("Citra");
+
+if (posisi !== -1) {
+    console.log("Citra ditemukan di index " + posisi)
+} else {
+    console.log("Citra tidak ditemukan");
+}
+
+
+let nama2 = ["Rahadian", "Andi", "Budi", "Citra"];
+
+nama2.forEach(function(nama2Sekarang) {
+    console.log("Halo " + nama2Sekarang + "!");
+});
+
+
+let nilai2 = [60, 80, 90, 55, 75];
+
+nilai2.forEach(function(nilai2Sekarang) {
+    if (nilai2Sekarang >= 75) {
+        console.log("Lulus!");
+    } else {
+        console.log("Tidak Lulus!");
+    }
+});
+
+
+let nilai3 = [65, 80, 95, 70, 88];
+
+nilai3.forEach(function(nilai3Sekarang) {
+    if (nilai3Sekarang >= 80) {
+        console.log(nilai3Sekarang + " -> Lulus!");
+    } else {
+        console.log(nilai3Sekarang + " -> Tidak Lulus!");
+    }
+});
